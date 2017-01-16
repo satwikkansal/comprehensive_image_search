ec74efd2c28f86bd04f5616380382944c8212a0a
satwikkansal <satwikkansal@gmail.com>
2017-01-17 01:49:34 +0530

    Removed unnecessary files

--- Bing_Image_search.py
+++ Bing_Image_search.py
@@ -1,7 +1,7 @@
 from py_bing_search import PyBingImageSearch as pbis 
 
 def bing_search(keywords,limit=10):
-	API_KEY = '1TWo2MkjPvqVDrJj0ssZTXAt1wJJfRX7GXoCmpnZ2XY'
+	API_KEY = 'YOUR_API_KEY'
 	search_query = (' ').join(keywords)
 	image_filters = image_filters='Size:medium' #optional
 	bi = pbis(API_KEY, search_query)
